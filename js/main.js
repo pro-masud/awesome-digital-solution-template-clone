51fddde651ef21a9d25af93d5b8719a2288a26c9
pro-mannan <promasudbd@gmail.com>
2023-08-01 16:19:45 +0600

    hero IT ADS section complete

--- js/main.js
+++ js/main.js
@@ -9,6 +9,18 @@
         $("#preloader").delay(100).fadeOut("fade");
     });
 
+    // 
+
+    $(window).on("scroll", function () {
+        var scroll = $(window).scrollTop();
+    
+        if (scroll < 2) {
+          $("nav.sticky-header").removeClass("affix");
+        } else {
+          $("nav.sticky-header").addClass("affix");
+        }
+      });
+
 
 
 
